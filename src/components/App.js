670ae223061ee47b92b36f25c9e9ab7d53460bda
enzoesper <enzoesper@hotmail.com>
2021-01-24 21:49:21 -0300

    initial files and first import of AppBar component for the Header

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './ui/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      App Component!
+    </div>
+  );
+}
+
+export default App;
