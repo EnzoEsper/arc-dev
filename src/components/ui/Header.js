a1ea4f36ba0768edb66d405ca1428b6010a325df
enzoesper <enzoesper@hotmail.com>
2021-01-25 02:40:46 -0300

    adding the favicon

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/styles';
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -20,7 +20,11 @@ function ElevationScroll(props) {
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
+    marginBottom: "3em"
+  },
+  logo: {
+    height: "7em"
   }
 }))
 
@@ -30,14 +34,12 @@ export default function Header(props) {
     <>
       <ElevationScroll>
         <AppBar position="fixed">
-          <Toolbar>
-          <Typography variant="h3">
-            Arc Development
-          </Typography>
+          <Toolbar disableGutters>
+            <img src={logo} alt="company logo" className={classes.logo}/>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
-      <div className={classes.toolbarMargin} />
+      <div className={classes.toolbarMargin}/>
     </>
   );
 }
