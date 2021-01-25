2f7e1387e5bc9269199135ac1a9ce89c28cb8b83
enzoesper <enzoesper@hotmail.com>
2021-01-25 03:10:19 -0300

    using tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,6 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
 import logo from '../../assets/logo.svg';
+import { Tab, Tabs } from '@material-ui/core';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -25,6 +26,14 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: "7em"
+  },
+  tabContainer: {
+    marginLeft: "auto"
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px"
   }
 }))
 
@@ -36,6 +45,13 @@ export default function Header(props) {
         <AppBar position="fixed">
           <Toolbar disableGutters>
             <img src={logo} alt="company logo" className={classes.logo}/>
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
