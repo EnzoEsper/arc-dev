16890cfa18bc78ea7ce8392f00408a9831196ebb
enzoesper <enzoesper@hotmail.com>
2021-01-24 22:00:57 -0300

    setting up Theme with the ThemeProvider component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
 import Header from './ui/Header';
+import theme from './ui/Theme';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
       App Component!
-    </div>
+    </ThemeProvider>
   );
 }
 
